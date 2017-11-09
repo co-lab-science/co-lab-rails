48eccbfc057d92408f2c43044fef02ac1ea7d3e4
Spencer Tang <stang@xogrp.com>
2017-11-09 16:11:52 -0300

    Add new pngs for content type

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.paths << Rails.root.join("app", "assets", "fonts")
   config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
