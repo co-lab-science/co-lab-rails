bd6ed81ed84e90fab81e068e753129e6bcd8ea6a
mrmitchboyer <mitch@mitchboyer.com>
2017-12-01 10:46:14 -0500

    Add styling to timeline

--- app/helpers/labs_helper.rb
+++ app/helpers/labs_helper.rb
@@ -6,7 +6,7 @@ module LabsHelper
   def find_lab_observations
     if @related_observations == []
       if lab_parent?
-        [Lab.find(@lab.parent)]
+        Lab.where(parent: @lab.id)
       else
         []
       end
@@ -18,7 +18,7 @@ module LabsHelper
   def find_lab_questions
     if @lab.questions == []
       if lab_parent?
-        Lab.find(@lab.parent).questions
+        Question.where(lab_id: @lab.id)
       else
         []
       end
@@ -28,7 +28,7 @@ module LabsHelper
   end
 
   def find_lab_hypotheses
-      @lab.hypotheses
+      Hypothesis.where(lab_id: @lab.id)
   end
 
   def lab_parent?
