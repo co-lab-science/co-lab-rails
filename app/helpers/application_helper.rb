de2da8fbf0a7a8c128edb977477dca3d0bd9a1e4
mrmitchboyer <mitch@mitchboyer.com>
2017-12-04 18:11:58 -0500

    adds upvote count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def upvote(post)
+    if post.upvotes.any? { |u| u.user_id == @current_user.id }
+    else
+      post.upvotes.new(user_id: @current_user.id)
+    end
+  end
+
+  def downvote(post)
+  end
 end
