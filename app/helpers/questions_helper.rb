8ec7716111d1e740260d40c0d9a667ad156fd528
mrmitchboyer <mitch@mitchboyer.com>
2017-12-05 11:05:33 -0500

    fix new question bug

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,41 +1,45 @@
 module QuestionsHelper
-  def find_question_questions
+  def find_question_questions(question)
     if @related_questions == []
-      if question_parent?
-        [Question.find(@question.parent)]
+      if question_parent?(question)
+        [Question.find(question.parent)]
       else
-        question_array
+        question_array(question)
       end
     else
       @related_questions
     end
   end
 
-  def find_question_hypotheses
-    if @question.hypotheses == []
-      find_parent_lab.hypotheses.compact
+  def find_question_hypotheses(question)
+    if question.hypotheses == []
+      find_parent_lab(question).hypotheses.compact
     else
-      @question.hypotheses
+      question.hypotheses
     end
   end
 
-  def find_question_observations
-    if @question.labs == []
-      [find_parent_lab]
+  def find_question_observations(question)
+    if question.labs == []
+      [find_parent_lab(question)].flatten
     else
-      @question.labs
+      question.labs
     end
   end
 
-  def question_parent?
-    @question.parent != nil
+  def question_parent?(question)
+    question.parent != nil
   end
 
-  def find_parent_lab
-    Lab.find(@question.lab_id)
+  def find_parent_lab(question)
+    if question.lab_id == nil
+      Lab.all.shuffle.first
+    else
+      Lab.find(question.lab_id)
+    end
   end
 
-  def question_array
-    find_parent_lab.questions.map { |q| q if q.id != @question.id }.compact
+  def question_array(question)
+    find_parent_lab(question).questions.map { |q| q if q.id != question.id }.compact
   end
 end
