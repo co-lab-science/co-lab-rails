4ea9c112425cbf9e6b3d0ca768f3daf4f98c4dab
github username <mitch@mitchboyer.com>
2017-12-06 10:52:26 -0500

    remove old helper methods

--- app/helpers/hypotheses_helper.rb
+++ app/helpers/hypotheses_helper.rb
@@ -1,33 +1,3 @@
 module HypothesesHelper
-  def find_hypothesis_hypotheses
-    if @related_hypotheses == []
-      if hypothesis_parent?
-        [Hypothsis.find(@hypothesis.parent)]
-      else
-        hypotheses_array
-      end
-    else
-      @related_hypotheses
-    end
-  end
 
-  def find_hypothesis_questions
-    find_hypothesis_parent_lab.questions.compact
-  end
-
-  def find_hypothesis_observations
-    [find_hypothesis_parent_lab]
-  end
-
-  def hypothesis_parent?
-    @hypothesis.parent != nil
-  end
-
-  def find_hypothesis_parent_lab
-    Lab.find(@hypothesis.lab_id)
-  end
-
-  def hypotheses_array
-    find_hypothesis_parent_lab.hypotheses.map { |h| h if h.id != @hypothesis.id }.compact
-  end
 end
